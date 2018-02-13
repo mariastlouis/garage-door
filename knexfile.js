bb7dfca6bbacafbba7306160af6b219ac9ac6823
mariastlouis <msantray@yahoo.com>
2018-02-12 17:43:00 -0700

    modified knex file

--- knexfile.js
+++ knexfile.js
@@ -23,13 +23,13 @@ test: {
       directory:'./db/seeds/test'
     },
     useNullAsDefault: true
-  },
- production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL + `?ssl=true`,
-    migrations: {
-      directory: './db/migrations'
-    },
-    useNullAsDefault: true
   }
+ // production: {
+ //    client: 'pg',
+ //    connection: process.env.DATABASE_URL + `?ssl=true`,
+ //    migrations: {
+ //      directory: './db/migrations'
+ //    },
+ //    useNullAsDefault: true
+ //  }
 };
